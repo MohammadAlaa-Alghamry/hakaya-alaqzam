3989fe761c101bfa6ae5880f0e2fbc464a41e918
MohammadAlaa-Alghamry <mohammad.alaa.alghamry@gmail.com>
2024-04-22 08:36:58 +0200

    revision 1

--- src/app/Compontents/Footer.tsx
+++ src/app/Compontents/Footer.tsx
@@ -1,70 +1,62 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-   faTwitter, faInstagram, faFacebookF
+    faTwitter, faInstagram, faFacebookF
 } from "@fortawesome/free-brands-svg-icons";
 
 
-export default function Footer(){
-    return(
-<div className="w-full  px-8 py-11 bg-[rgb(246,235,223)]">
-<div className="container flex justify-center align-middle flex-col gap-6 ">
+export default function Footer() {
+    return (
+        <div className="w-full  px-8 py-11 bg-[rgb(246,235,223)]">
+            <div className="container flex justify-center align-middle flex-col gap-6 ">
 
-<ul className="flex justify-around align-middle gap-5 mx-auto md:w-[50%]  ">
-    <li >
-        <Link href={""} className="text-[rgb(127,131,65)] ">Contact us</Link>
-    </li>
-    <li>
-        <Link href={""} className="text-[rgb(127,131,65)] ">About us</Link>
-    </li>
-    <li>
-        <Link href={""}className="text-[rgb(127,131,65)] ">Copy rights</Link>
-    </li>
-</ul>
+                <ul className="flex justify-around align-middle gap-5 mx-auto md:w-[50%]  ">
+                    <li >
+                        <Link href={""} className="text-[rgb(127,131,65)] ">Contact us</Link>
+                    </li>
+                    <li>
+                        <Link href={""} className="text-[rgb(127,131,65)] ">About us</Link>
+                    </li>
+                    <li>
+                        <Link href={""} className="text-[rgb(127,131,65)] ">Copy rights</Link>
+                    </li>
+                </ul>
 
-<ul className="flex justify-around align-middle gap-5 mx-auto md:w-[30%]  ">
-    <li style={{
-        backgroundColor: 'rgb(127, 131, 65)',
-        display: 'flex',
-        height: '35px',
-        width: '35px',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: '50%',
-      }}>
-        <Link href={""}  >  
-        <FontAwesomeIcon icon={faInstagram} />
-      
-        </Link>
-    </li>
-    <li style={{
-        backgroundColor: 'rgb(127, 131, 65)',
-        display: 'flex',
-        height: '35px',
-        width: '35px',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: '50%',
-      }}>
-        <Link href={""} >
-        <FontAwesomeIcon icon={faTwitter} />
-        </Link>
-    </li>
-    <li style={{
-        backgroundColor: 'rgb(127, 131, 65)',
-        display: 'flex',
-        height: '35px',
-        width: '35px',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: '50%',
-      }}  >
-        <a href={""}>
-        <FontAwesomeIcon icon={faFacebookF} />
-        </a>
-    </li>
-</ul>
-</div>
-</div>
+                <ul className="flex justify-around align-middle gap-5 mx-auto md:w-[30%]  ">
+                    <li className="flex bg-slate-600 w-[]">
+                        <Link href={""}  >
+                            <FontAwesomeIcon icon={faInstagram} />
+
+                        </Link>
+                    </li>
+                    <li style={{
+                        backgroundColor: 'rgb(127, 131, 65)',
+                        display: 'flex',
+                        height: '35px',
+                        width: '35px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: '50%',
+                    }}>
+                        <Link href={""} >
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </Link>
+                    </li>
+                    <li style={{
+                        backgroundColor: 'rgb(127, 131, 65)',
+                        display: 'flex',
+                        height: '35px',
+                        width: '35px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: '50%',
+                    }}  >
+                        <a href={""}>
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import Nav from "./Compontents/Nav";
-import Section1 from "./Compontents/Section1";
-import Section2 from "./Compontents/Section2";
-import Section3 from "./Compontents/Section3";
+import Section1 from "./Compontents/home/Section1";
+import Section2 from "./Compontents/home/Section2";
+import Section3 from "./Compontents/home/Section3";
 import Footer from "./Compontents/Footer";
 
 export default function Home() {
