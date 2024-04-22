90e157032bf66339fd2b6c8097084ee9b29b5636
MohammadAlaa-Alghamry <mohammad.alaa.alghamry@gmail.com>
2024-04-22 16:49:18 +0200

    revision 2

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <main >
       <Nav></Nav>
-    <AboutUs></AboutUs>
+      <AboutUs></AboutUs>
       <OurProducts></OurProducts>
       <Footer></Footer>
     </main>
