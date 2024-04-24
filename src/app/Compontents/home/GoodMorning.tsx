80392b4342a795f82a219e3161757d8a3521f242
MohammadAlaa-Alghamry <mohammad.alaa.alghamry@gmail.com>
2024-04-24 18:49:52 +0200

    fixing mobile homepage

--- src/app/Compontents/home/GoodMorning.tsx
+++ src/app/Compontents/home/GoodMorning.tsx
@@ -5,9 +5,9 @@ export default function GoodMorning() {
     return (
         <div
             style={{ backgroundImage: 'url("/img/home/hero-img.png")' }}
-            className="h-full w-full flex justify-center items-center bg-cover bg-left-top aspect-[14/21] sm:aspect-[21/13] p-10">
+            className="h-full w-full flex justify-center items-center bg-cover bg-left-top aspect-[14/21] sm:aspect-[21/13] sm:p-10">
 
-            <div className="w-full flex flex-col sm:flex-row  justify-between items-center h-full">
+            <div className="w-full flex flex-col sm:flex-row  sm:justify-between items-center h-full">
                 <div className="flex justify-start items-center flex-col w-full h-[full] md:w-[50%] gap-7">
                     <div className="text-[rgb(127,131,65)] text-center text-2xl sm:text-4xl md:text-5xl">
                         <h1>Good Morning To</h1>
@@ -17,7 +17,7 @@ export default function GoodMorning() {
                     <div 
                     className="open-qutation-mobile sm:open-qutation bg-right-top bg-no-repeat w-[20%]  ">
                 </div>
-                    <p className="w-[60%] text-center text-[rgb(62,54,37)] text-xl "> Lorem ipsum dolor sit amet consectetur. Scelerisque egestas risus velit quisque ut iaculis magnis et. A purus libero volutpat ultricies tortor gravida donec. </p>
+                    <span className="w-[60%] text-center text-[rgb(62,54,37)] text-sm sm:text-xl "> Lorem ipsum dolor sit amet consectetur. Scelerisque egestas risus velit quisque ut iaculis magnis et. A purus libero volutpat ultricies tortor gravida donec. </span>
                    
                     <div
                     className="closed-qutation-mobile sm:closed-qutation bg-left-bottom bg-no-repeat w-[20%]  ">
