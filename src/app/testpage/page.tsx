96260d8a66a8238d5b0cb2fffe4aa9dbd94e828e
MohammadAlaa-Alghamry <mohammad.alaa.alghamry@gmail.com>
2024-04-26 23:35:04 +0300

    investigating images issue - solved

---
+++ src/app/testpage/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function Page() {
+    return (
+        // <main className="h-full w-[1440px] max-w-[1440px] min-w-[1440px] aspect-[3/3]">
+        //  <main className="h-full w-[1920px] max-w-[1920px]">
+        <main className="h-full w-full flex justify-center items-center">
+                <div
+                    className="bg-[url('/img/SignUp/sign-up-bg-mobile.png')] md:bg-[url('/img/SignUp/Sign-up.png')] w-[100%] h-[100%] bg-center bg-contain bg-no-repeat">
+                </div>
+        </main>
+    );
+}
