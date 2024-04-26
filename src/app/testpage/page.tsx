8d778f8fee93ee12b6a1da662f1d7c07be121a35
MohammadAlaa-Alghamry <mohammad.alaa.alghamry@gmail.com>
2024-04-27 01:34:48 +0300

    fixed 'the fixed hight issue' in the signup page

--- src/app/testpage/page.tsx
+++ src/app/testpage/page.tsx
@@ -4,10 +4,16 @@ export default function Page() {
     return (
         // <main className="h-full w-[1440px] max-w-[1440px] min-w-[1440px] aspect-[3/3]">
         //  <main className="h-full w-[1920px] max-w-[1920px]">
-        <main className="h-full w-full flex justify-center items-center">
+        <div className="w-full flex flex-col justify-center items-center">
                 <div
-                    className="bg-[url('/img/SignUp/sign-up-bg-mobile.png')] md:bg-[url('/img/SignUp/Sign-up.png')] w-[100%] h-[100%] bg-center bg-contain bg-no-repeat">
+                    className="bg-[url('/img/SignUp/sign-up-bg-mobile.png')] md:bg-[url('/img/SignUp/Sign-up.png')] w-[100%] aspect-[1.5] bg-center bg-contain bg-no-repeat">
                 </div>
-        </main>
+                <div
+                    className="bg-[url('/img/SignUp/sign-up-bg-mobile.png')] md:bg-[url('/img/SignUp/Sign-up.png')] w-[100%] aspect-[1.5] bg-center bg-contain bg-no-repeat">
+                </div>
+                <div
+                    className="bg-[url('/img/SignUp/sign-up-bg-mobile.png')] md:bg-[url('/img/SignUp/Sign-up.png')] w-[100%] aspect-[1.5] bg-center bg-contain bg-no-repeat">
+                </div>
+        </div>
     );
 }
